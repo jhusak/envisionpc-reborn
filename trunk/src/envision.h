d6d7d93348dced8665da31f41feb80775e1144e5
jakub.husak@gmail.com <jakub.husak@gmail.com@7cdeb40b-c5dd-7c33-eb31-1031addba7da>
2011-02-03 00:13:35 +0000

    replaced range and box checking to more convinient

--- trunk/src/envision.h
+++ trunk/src/envision.h
@@ -22,6 +22,9 @@ typedef struct view {
   unsigned char *map;
 } view;
 
+#define IN_RANGE(x,lx,rx) ((x)>=(lx)&&(x)<=(rx))
+#define IN_BOX(x,y,lx,rx,ly,ry) (IN_RANGE((x),(lx),(rx))&&IN_RANGE((y),(ly),(ry)))
+
 int title();
 
 int get_8x8_mode(int m);
